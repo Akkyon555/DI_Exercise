f585999005fbe67bda56631c088d887a210ea2d1
= <=>
2020-09-03 19:59:23 +0200

    Week 3 Update

---
+++ WEEK_3/day 1/exo 2/indexjs.js
@@ -0,0 +1,5 @@
+let str = "I am looking for Nemo";
+let str_array = str.split(" ");
+	console.log(str_array);
+let position = str_array.indexOf("Nemo");
+	console.log("I found Nemo at " + position);
